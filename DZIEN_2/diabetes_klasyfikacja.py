1ee07de85279b428cda9b5d7941c185d7afb08db
albim72 <marcin.albiniak72@gmail.com>
2023-07-04 11:07:06 +0200

    Update diabetes_klasyfikacja.py

--- DZIEN_2/diabetes_klasyfikacja.py
+++ DZIEN_2/diabetes_klasyfikacja.py
@@ -7,3 +7,17 @@ col_names = ['pregnant','glucose','bp','skin','insulin','bmi','pedigree','age','
 
 pima = pd.read_csv("diabetes.csv",header=None,names=col_names)
 print(pima.head())
+
+feature_cols = ['pregnant','insulin','bmi','age','glucose','bp','pedigree']
+X = pima[feature_cols]
+y = pima.label
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=1)
+
+clf = DecisionTreeClassifier()
+
+clf = clf.fit(X_train,y_train)
+
+y_pred = clf.predict(X_test)
+
+print(f"Dokładność (Accuracy): {metrics.accuracy_score(y_test,y_pred)}")
